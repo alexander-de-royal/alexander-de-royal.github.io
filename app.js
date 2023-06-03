daa434a67f155d034aeb1f99bbac8a8ee93faf5b
Alexander Royal <alexander.de.royal@gmail.com>
2023-06-03 07:35:52 -0500

    added a blurring background

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-//toggle icon navbar
+// LOADING TEXT
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+
+let load = 0;
+
+let int = setInterval(blurring, 30); // 30ms
+
+function blurring() {
+    load++
+
+    if(load > 99) {
+        clearInterval(int)
+    }
+
+    loadText.innerText = `${load}%`;
+    // text fades out as it loads from 0 to 100 (opacity goes from '1' to '0')
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    // blur starts at 30px and ends at 0
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+}
+
+// TOGGLE ICON NAVBAR
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
